f980fe64eed834ca03d2c3fb273a4889acff6f07
Jaclyn Sunardi <jaclyn.sunardi@yahoo.ca>
2024-01-20 12:50:10 -0800

    background

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
-import {Helmet} from 'react-helmet';
-
+import logo from './logo.svg';
 import './App.css';
 
-class Application extends React.Component {
+class MyHeader extends React.Component {
   render () {
+    const mystyle = {
+      color: "blue",
+      backgroundColor: "blue",
+      padding: "10px",
+      fontFamily: "Arial"
+    };
     return (
-      <div className='application'>
-        <Helmet>
-          <style>{'body { background-color: red; }'}</style>
-        </Helmet>
+      <div>
+        <h1> Drip Check</h1>
       </div>
     )
   }
@@ -20,39 +23,14 @@ function App() {
   const [location, setLocation] = useState(null);
   const [weather, setWeather] = useState(null);
   
-  function handleSubmit() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(getLocation, error);
-    } else {
-      console.log("Geolocation not supported");
-    }
-  }
   
-  function getLocation(position) {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-    setLocation({ latitude, longitude });
-    console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
-
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=f1fd5922d1b9618b48fa043483a37ba1&units=metric`)
-      .then(response => response.json())
-      .then(data => {
-        setWeather(data);
-        console.log(data);
-      })
-      .catch(error => console.log(error));
-  }
-
-  function error() {
-    console.log("Unable to retrieve your location");
+  function handleSubmit() {
+    
   }
   
   return (
     <div className="App">
       <h1>Drip Check</h1>
-      <div>
-        <button onClick={handleSubmit}>Get Location</button>
-      </div>
     </div>
   );
 }
