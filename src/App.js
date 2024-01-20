edfdce8fc00e04c281d0ead1c4b530d5cd53742e
ChonkyChonker <louisjyluo@gmail.com>
2024-01-20 14:03:57 -0800

    changed search bar size and button size

--- src/App.js
+++ src/App.js
@@ -1,23 +1,5 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-class MyHeader extends React.Component {
-  render () {
-    const mystyle = {
-      color: "blue",
-      backgroundColor: "blue",
-      padding: "10px",
-      fontFamily: "Arial"
-    };
-    return (
-      <div>
-        <h1> Drip Check</h1>
-      </div>
-    )
-  }
-}
-
 
 function App() {
   const [location, setLocation] = useState(null);
@@ -35,8 +17,8 @@ function App() {
         <h1> Check The Drip &#9748;</h1>
       </div>
       <div className='Location'>
-        <input></input>
-        <button onClick={handleSubmit}>Get Location</button>
+        <input id="searchBar"></input>
+        <button id='getLocation' onClick={handleSubmit}>Get Location</button>
       </div>
     </div>
   );
