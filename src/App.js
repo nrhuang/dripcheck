412fea199d2e722f82c9fc07093c62bb69a7f80a
nrhuang <nhuangra@gmail.com>
2024-01-20 12:19:54 -0800

    added get location button with weather api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -8,12 +7,38 @@ function App() {
   const [weather, setWeather] = useState(null);
   
   function handleSubmit() {
-    
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(getLocation, error);
+    } else {
+      console.log("Geolocation not supported");
+    }
+  }
+  
+  function getLocation(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    setLocation({ latitude, longitude });
+    console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=f1fd5922d1b9618b48fa043483a37ba1&units=metric`)
+      .then(response => response.json())
+      .then(data => {
+        setWeather(data);
+        console.log(data);
+      })
+      .catch(error => console.log(error));
+  }
+
+  function error() {
+    console.log("Unable to retrieve your location");
   }
   
   return (
     <div className="App">
       <h1>Drip Check</h1>
+      <div>
+        <button onClick={handleSubmit}>Get Location</button>
+      </div>
     </div>
   );
 }
