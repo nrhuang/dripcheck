bc02ef9300dd24d072280f2541870c1d023bde03
Jaclyn Sunardi <jaclyn.sunardi@yahoo.ca>
2024-01-20 21:47:41 -0800

    unchanged

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Loader } from "@googlemaps/js-api-loader"
 import './App.css';
-
-const loader = new Loader({
-  apiKey: process.env.REACT_APP_MAP_API_KEY,
-  version: "weekly",
-  libraries: ["maps"]
-});
 
 function App() {
   const [location, setLocation] = useState(null);
@@ -16,31 +9,6 @@ function App() {
   const [image, setImage] = useState(null);
   const [hasImage, setHasImage] = useState(false);
   const [placeholderText, setPlaceholderText] = useState('');
-  const [googleMapState, setGoogleMapState] = useState(null);
-
-  let googleMapDiv;
-
-  useEffect(() => {
-        const defaultMapOptions = {
-            center: {
-                lat: 40.762312,
-                lng: -73.979345
-            },
-            zoom: 11
-        };
-        loader.load().then((google) => {
-            const map = new google.maps.Map(
-                googleMapDiv,
-                defaultMapOptions);
-            map.addListener("click", (e) => {
-              console.log(e.latLng.lat() + " " + e.latLng.lng());
-            })
-            setGoogleMapState({
-                google: google,
-                map: map
-            });
-        });
-  },[weather])
 
   useEffect(() => {
     generateRandomPlaceholder();
@@ -53,17 +21,8 @@ function App() {
     setPlaceholderText(randomItem);
   }
   
-  useEffect(() => {
-    getLocation();
-  },[]);
-
-  useEffect(()=> {
-    getWeather();
-  },[location])
-  
   function handleSubmit() {
-    getInput();
-    createPrompt();
+    getLocation()
   }
   
   function getLocation() {
@@ -82,15 +41,32 @@ function App() {
     }
   }
   
+  useEffect(() => {
+    createPrompt();
+    console.log('got the weather!!!');
+  }, [weather]);
+  
+  function handleSubmit() {
+    getWeather();
+    getInput();
+  }
+  
   function getWeather() {
+    console.log(location);
     if(!hasLocation) return;
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}&units=metric`)
       .then(response => response.json())
       .then(data => {
         setWeather(data);
         setHasWeather(true);
+        console.log(data);
       })
       .catch(error => console.log(error));
+  }
+  
+  function createPrompt() {
+    console.log(location);
+    console.log(weather);
   }
 
   function getInput() {
@@ -102,27 +78,22 @@ function App() {
     else if(input.value === 'gullible') setImage("https://media1.tenor.com/m/DABdHr-IoaAAAAAC/cat-gullible.gif");
   }
   
-  function createPrompt() {
-    console.log(location);
-    console.log(weather);
-  }
-  
   return (
     <div className="App">
       <head>
         <link rel="icon" href="%PUBLIC_URL%/dripcheck.ico" />
         <title>Drip Check</title>
       </head>
-      <div ref={(ref) => { googleMapDiv = ref }}
-        style={{ height: '50vh', width: '50vh' }}>
-      </div>
       <img id="logo" src="dripcheck.png" alt= "logo"/>  
       <div className='Title'>
-        <h1 id='header'> Check The Drip &#9748;</h1>
+        <h1> Check The Drip &#9748;</h1>
+      </div>
+      <div className='Instructions'>
+        <p> </p>
       </div>
       <div className='Location'>
         <input id="searchBar" placeholder={placeholderText}></input>
-        <button id='getDrip' onClick={handleSubmit} disabled={!hasLocation && !hasWeather}>Get My Drip</button>
+        <button id='getDrip' onClick={handleSubmit}>Get My Drip</button>
       </div>
       <div className='dripImage'> 
         {hasLocation && hasWeather && hasImage && <img id= 'drip' src={image} alt="filler"></img>}
