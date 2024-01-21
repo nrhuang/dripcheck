c9730a4e121ffbefb631b32dd5d5f75082a04d29
ChonkyChonker <louisjyluo@gmail.com>
2024-01-21 02:29:42 -0800

    Added toggle switch and positions
    Added A location bar and switch that changes the location based on the toggled switch state you're currently on.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Component } from 'react';
+import Switch from "react-switch";
 import { Loader } from "@googlemaps/js-api-loader"
 import './App.css';
 
@@ -10,13 +11,22 @@ const loader = new Loader({
 
 function App() {
   const [location, setLocation] = useState(null);
-  const [weather, setWeather] = useState(null);
   const [hasLocation, setHasLocation] = useState(false);
+
+  const [mapLocation, setMapLocation] = useState([49.262152, -123.244766]);
+  const [hasMapLocation, setHasMapLocation] = useState(false);
+
+  const [weather, setWeather] = useState(null);
   const [hasWeather, setHasWeather] = useState(false);
+
   const [image, setImage] = useState(null);
   const [hasImage, setHasImage] = useState(false);
+
+  const [currLocation, setCurrLocation] = useState(null);
+
   const [placeholderText, setPlaceholderText] = useState('');
   const [googleMapState, setGoogleMapState] = useState(null);
+  const [checked, setChecked] = useState(false);
 
   let googleMapDiv;
 
@@ -34,6 +44,10 @@ function App() {
                 defaultMapOptions);
             map.addListener("click", (e) => {
               console.log(e.latLng.lat() + " " + e.latLng.lng());
+              var lat = e.latLng.lat();
+              var lng = e.latLng.lng();
+              setMapLocation([lat, lng]);
+              setHasMapLocation(true);
             })
             setGoogleMapState({
                 google: google,
@@ -64,6 +78,12 @@ function App() {
   function handleSubmit() {
     getInput();
     createPrompt();
+    if(checked) {
+      setCurrLocation(location);
+    } else {
+      setCurrLocation(mapLocation);
+    }
+    console.log("current location is: " + currLocation);
   }
   
   function getLocation() {
@@ -71,7 +91,7 @@ function App() {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           const { latitude, longitude } = position.coords;
-          setLocation({ latitude, longitude });
+          setLocation([latitude, longitude]);
           setHasLocation(true);
         },
         (error) => {
@@ -84,7 +104,7 @@ function App() {
   
   function getWeather() {
     if(!hasLocation) return;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}&units=metric`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${currLocation.latitude}&lon=${currLocation.longitude}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}&units=metric`)
       .then(response => response.json())
       .then(data => {
         setWeather(data);
@@ -103,8 +123,22 @@ function App() {
   }
   
   function createPrompt() {
-    console.log(location);
+    console.log(currLocation);
     console.log(weather);
+  }
+
+  function handleLocation() {
+    if(hasLocation) {
+      setChecked(false);
+      setCurrLocation(mapLocation);
+      setHasLocation(false);
+      setHasMapLocation(true);
+    } else {
+      setChecked(true);
+      setCurrLocation(location);
+      setHasLocation(true);
+      setHasMapLocation(false);
+    }
   }
   
   return (
@@ -113,23 +147,47 @@ function App() {
         <link rel="icon" href="%PUBLIC_URL%/dripcheck.ico" />
         <title>Drip Check</title>
       </head>
+      
       <img id="logo" src="dripcheck.png" alt= "logo"/>  
+
       <div className='Title'>
         <h1 id='header'> Check The Drip &#9748;</h1>
       </div>
+
       <div className="Map" ref={(ref) => { googleMapDiv = ref }}
         style={{ height: '35vh', width: '67vh' }}>
       </div>
+
       <div className='CurrPos'>
-        <h2>meow</h2>
+        <p id>{checked ? "Lat: "+ Math.round(location[0] * 1000000) / 1000000 + "Long: "+ Math.round(location[1] * 1000000) / 1000000: "Lat: "+ Math.round(mapLocation[0] * 1000000) / 1000000 + "Long: "+ Math.round(mapLocation[1] * 1000000) / 1000000} </p>
       </div>
+
+      <div className='Switch'>
+        <span id='switch'>{checked? "Reading Current Location: ":" Reading Map Location: "}</span>
+        <Switch 
+          className='toggleSwitch'
+          onColor="#86d3ff"
+          onHandleColor="#2693e6"
+          handleDiameter={30}
+          uncheckedIcon={false}
+          checkedIcon={false}
+          boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+          activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+          height={20}
+          width={48}
+          onChange={handleLocation} 
+          checked={checked}/>
+      </div>
+
       <div className='Location'>
         <input id="searchBar" placeholder={placeholderText}></input>
         <button id='getDrip' onClick={handleSubmit} disabled={!hasLocation && !hasWeather}>Get My Drip</button>
       </div>
+
       <div className='dripImage'> 
-        {hasLocation && hasWeather && hasImage && <img id= 'drip' src={image} alt="filler"></img>}
+        {(hasLocation || hasMapLocation) && hasWeather && hasImage && <img id= 'drip' src={image} alt="filler"></img>}
       </div>
+
     </div>
   );
 }
