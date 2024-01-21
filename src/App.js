06b184d7a1823db83678043882a7205fed77dfd9
Jaclyn Sunardi <jaclyn.sunardi@yahoo.ca>
2024-01-20 16:18:31 -0800

    background

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import './App.css';
 function App() {
   const [location, setLocation] = useState(null);
   const [weather, setWeather] = useState(null);
-  const [hasLocation, setHasLocation] = useState(false);
-  const [image, setImage] = useState(null);
-  const [hasImage, setHasImage] = useState(false);
   const [placeholderText, setPlaceholderText] = useState('');
 
   useEffect(() => {
@@ -14,19 +11,22 @@ function App() {
   },[])
 
   function generateRandomPlaceholder(){
-    const items = ["Give me something stylish...","What should I wear to a red carpet event?", "What should I wear to a party?"];
+    const items = ["Give me something stylish...","What should I wear to a red carpet event?",
+                  "I want to try something new...", "It's raining but I still want to look good."];
     const randomItem = items[Math.floor(Math.random() * items.length)];
     setPlaceholderText(randomItem);
   }
-
-  useEffect(() => {
+  
+  function handleSubmit() {
+    getLocation()
+  }
+  
+  function getLocation() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           const { latitude, longitude } = position.coords;
           setLocation({ latitude, longitude });
-          setHasLocation(true);
-          console.log('got the location!!!');
         },
         (error) => {
           console.log('Error getting current location: ', error);
@@ -34,44 +34,15 @@ function App() {
     } else {
       console.log("Geolocation not supported");
     }
-  }, []);
-  
-  useEffect(() => {
-    createPrompt();
-  }, [weather]);
-  
-  function handleSubmit() {
-    getWeather();
-    getInput();
-  }
-  
-  function getWeather() {
     console.log(location);
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}&units=metric`)
-      .then(response => response.json())
-      .then(data => {
-        setWeather(data);
-        console.log(data);
-      })
-      .catch(error => console.log(error));
-  }
-  
-  function createPrompt() {
-    console.log(location);
-    console.log(weather);
-  }
-
-  function getInput() {
-    setHasImage(true);
-    var input = document.getElementById("searchBar");
-    console.log(input.value);
-    if(input.value === 'meow') setImage("https://media1.tenor.com/m/Jc9jT66AJRwAAAAd/chipi-chipi-chapa-chapa.gif");
-    else if(input.value === 'gojo') setImage("https://i.redd.it/s85ejs5xxz4c1.gif");
-    else if(input.value === 'gullible') setImage("https://media1.tenor.com/m/DABdHr-IoaAAAAAC/cat-gullible.gif");
   }
   
   return (
     <div className="App">
+      <head>
+        <link rel="icon" href="%PUBLIC_URL%/dripcheck.ico" />
+        <title>Drip Check</title>
+      </head>
       <img id="logo" src="dripcheck.png" alt= "logo"/>  
       <div className='Title'>
         <h1> Check The Drip &#9748;</h1>
@@ -81,7 +52,7 @@ function App() {
         <button id='getLocation' onClick={handleSubmit}>Get Location</button>
       </div>
       <div className='dripImage'> 
-        {hasImage && <img id= 'drip' src={image} alt="filler"></img>}
+        <img id= 'drip' src="https://media1.tenor.com/m/Jc9jT66AJRwAAAAd/chipi-chipi-chapa-chapa.gif" alt="filler"></img>
       </div>
     </div>
   );
